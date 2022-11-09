0a14ea9350dc8d050c800f0ca3f74dd760cf7f60
Hein02 <117172810+Hein02@users.noreply.github.com>
2022-11-08 18:08:19 -0800

    Plan with pseudo code

---
+++ index.js
@@ -0,0 +1,15 @@
+// Ask user to type one of the following: 'rock', 'paper', 'scissors'
+//   Assign the user's input to a variable named userPick
+// Create a variable named computerPick
+// Generate a random number ranging from 0 to 2
+// Assign one of the following: 'rock', 'paper', 'scissors' to computerPick, based on the following conditions
+//   IF randomNumber = 0, assign 'rock' to computerPick
+//   ELSE IF randomNumber = 1, assign 'paper' to computerPick
+//   ELSE, assign 'scissors' to computerPick
+// Evaluate whether user wins or ties based on the inputs and print the result
+//   IF userPick = 'rock' AND computerPick = 'scissors'
+//     OR userPick = 'paper' AND computerPick = 'rock'
+//     OR userPick = 'scissors' AND computerPick = 'paper',
+//     print 'User wins'
+//   ELSE IF userPick = computerPick, print 'Ties'
+//   ELSE, print 'User loses'
